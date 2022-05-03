27908054f896a454bfedec676da00da6127b5c91
Arjuniest <thedivinetreee@gmail.com>
2022-05-03 19:49:59 +0530

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let repeatString = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        repeatString = word + repeatString;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
