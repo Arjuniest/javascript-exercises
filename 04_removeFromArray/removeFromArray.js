cb3f2bcf70b11196d02b634e3ce845c09cfe5c46
Arjuniest <thedivinetreee@gmail.com>
2022-05-03 19:50:50 +0530

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) { //taking arguments as a rest parameter.
+    // method one: 
+    // const array = args[0];
+    // return array.filter(val => !args.includes(val))
 
-};
+    const array = args[0];
+    const newArray = [];
+    //use forEach to go through the array
+    array.forEach((item) => {
+        //pushing every element to the new array
+        if (!args.includes(item)){
+            newArray.push(item);
+        };
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
