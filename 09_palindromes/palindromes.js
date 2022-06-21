23ae4410fa7d9ada1f22dff55b5e01df10e360bf
Arjuniest <thedivinetreee@gmail.com>
2022-06-21 20:11:04 +0530

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    wordsNew = words.toLowerCase().replace(/[^a-z]/g, "");
+    let reverse ='';
 
+    for (let i = words.length -1 ; i >= 0; i--) {
+        reverse += wordsNew.charAt(i);        
+    }
+    if( reverse === wordsNew){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
