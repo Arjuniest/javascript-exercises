5d942d46d788aa1eb8f3ea332a53ba048e4b944a
Arjuniest <thedivinetreee@gmail.com>
2022-06-21 19:51:54 +0530

    Add solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let current = 0;
+  for(let i = 0; i < array.length; i++){
+    current += array[i];
+  }
+  return current;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let current = 1;
+  for (let index = 0; index < array.length; index++) {
+     current = current * array[index]; }
+    return current;
+  };
 
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+  let product = 1;
+  let count = 1;
+  while (count <= num){
+    product = count * product;
+    count++;
+  }
+  return product;
 };
 
 // Do not edit below this line
